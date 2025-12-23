95af3c5af7f87b9e19f805b33d0f0337af8e38d1
EySoyIsaaa <129011075+EySoyIsaaa@users.noreply.github.com>
2025-12-23 16:25:46 -0500

    Config: Inicializar Capacitor para Android/iOS (versión móvil)

---
+++ android/app/src/main/java/com/eysayisaaa/epicenterdsp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eysayisaaa.epicenterdsp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eysayisaaa.epicenterdsp',
+  appName: 'Epicenter DSP Movil',
+  webDir: 'dist'
+};
+
+export default config;
